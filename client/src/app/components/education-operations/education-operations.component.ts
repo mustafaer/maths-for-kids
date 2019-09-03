761f7dfcd919ad9c08c2653a8fd8dd57a2a4619f
Mustafa ER <mustafaer@protonmail.com>
2019-09-04 00:21:53 +0300

    fix, prod

--- client/src/app/components/education-operations/education-operations.component.ts
+++ client/src/app/components/education-operations/education-operations.component.ts
@@ -15,17 +15,21 @@ export class EducationOperationsComponent implements OnInit {
   educationBackground = Items.educationBackground;
   apple = Items.apple;
   plate = Items.plate;
-  appleCount = 5;
-  appleCount2 = 3;
+  appleCount: number;
+  appleCount2: number;
   plusOperations = plus;
   isFirst: boolean;
   isLast: boolean;
   currentQuestion: number;
+  movedApple: number;
 
   constructor() {
+    this.movedApple = 0;
     this.isFirst = true;
     this.isLast = false;
     this.currentQuestion = 1;
+    this.appleCount = this.plusOperations[this.currentQuestion - 1].first;
+    this.appleCount2 = this.plusOperations[this.currentQuestion - 1].second;
   }
 
   ngOnInit() {
@@ -35,16 +39,22 @@ export class EducationOperationsComponent implements OnInit {
   sortApples() {
     // @ts-ignore
     $('#sortable1, #sortable2, #sortable3').sortable({
-      connectWith: '.connectedSortable',
+      connectWith: '#sortable3',
     }).on('sortstop', (event, ui) => {
+      this.movedApple++;
 
-      window.navigator.vibrate(200);
-
-    });
+    }).disableSelection();
   }
 
   nextQuestion() {
+    // @ts-ignore
+    $('.fruits3 .apple').remove();
     this.currentQuestion++;
+
+    this.appleCount = this.plusOperations[this.currentQuestion - 1].first;
+    this.appleCount2 = this.plusOperations[this.currentQuestion - 1].second;
+    this.createFruits1();
+    this.createFruits2();
     if (this.currentQuestion >= 10) {
       this.isLast = true;
     }
@@ -53,10 +63,38 @@ export class EducationOperationsComponent implements OnInit {
   }
 
   previousQuestion() {
+    // @ts-ignore
+    $('.fruits3 .apple').remove();
     this.currentQuestion--;
+    this.appleCount = this.plusOperations[this.currentQuestion - 1].first;
+    this.appleCount2 = this.plusOperations[this.currentQuestion - 1].second;
+    this.createFruits1();
+    this.createFruits2();
     if (this.currentQuestion <= 1) {
       this.isFirst = true;
     }
     this.isLast = false;
+  }
+
+  createFruits1() {
+    let x = 0;
+    let htmlImg = '';
+    while (x < this.appleCount) {
+      htmlImg = htmlImg + '<img class="apple fruits2_a" src="' + this.apple + '">';
+      x++;
+    }
+    // @ts-ignore
+    $('#sortable1').html(htmlImg);
+  }
+
+  createFruits2() {
+    let x = 0;
+    let htmlImg = '';
+    while (x < this.appleCount2) {
+      htmlImg = htmlImg + '<img class="apple fruits2_a" src="' + this.apple + '">';
+      x++;
+    }
+    // @ts-ignore
+    $('#sortable2').html(htmlImg);
   }
 }
